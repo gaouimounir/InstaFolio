7220a816fe6a8d8cda8d51801b8a429fc2c4db93
gaouimounir <mounir.gaoui@outlook.com>
2024-02-13 17:16:50 +0100

    feat: :art: construction architecture

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="homepage__header"></div>
+      <div className="homepage__skills"></div>
+      <div className="homepage__timeline"></div>
+    </div>
+  )
+}
+
+export default Homepage
