e01eebbaf8e4bbe5c8db82a5a24c8f832495df22
gaouimounir <mounir.gaoui@outlook.com>
2024-02-13 17:22:04 +0100

    feat: :pushpin: utilisation des components dans le projet

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Homepage.css'
+import Header from './header/Header'
+import Timeline from './timeline/Timeline'
+import Skills from './skills/Skills'
 
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="homepage__header"></div>
-      <div className="homepage__skills"></div>
-      <div className="homepage__timeline"></div>
+      <div className="homepage__header">
+        <Header />
+      </div>
+      <div className="homepage__skills">
+        <Skills />
+      </div>
+      <div className="homepage__timeline">
+        <Timeline />
+      </div>
     </div>
   )
 }
