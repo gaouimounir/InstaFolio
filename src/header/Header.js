936baf8a1bb30ec5def3a4cfe05fe8092e5caf0a
gaouimounir <mounir.gaoui@outlook.com>
2024-02-15 19:35:50 +0100

    feat: :art: update css header

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,17 +6,20 @@ import ImgProfil from "../image/ImgProfil.jpg";
 function Header() {
   return (
     <div className="header">
-      <div className="header__contact">
+      <div className="header__nav">
         <img className="header__img" src={ImgProfil} alt="" />
-        <a href="https://www.linkedin.com/in/am-gaoui/">
-          <i className="fa-brands fa-linkedin"></i>
-        </a>
-        <a href="mailto:am.gaoui@protonmail.com">
-          <i className="fa-solid fa-envelope"></i>
-        </a>
-        <a href="https://github.com/gaouimounir">
-          <i className="fa-brands fa-github"></i>
-        </a>
+
+        <div className="header__contact">
+          <a href="https://www.linkedin.com/in/am-gaoui/">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="mailto:am.gaoui@protonmail.com">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+          <a href="https://github.com/gaouimounir">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
       </div>
       <div className="header__presentation">
         <div className="header__name">
