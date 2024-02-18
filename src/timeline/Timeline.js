f84c01a5d37a49e257cf1aa6192c8dd032fc2860
gaouimounir <mounir.gaoui@outlook.com>
2024-02-18 22:19:51 +0100

    feat: :art: affichage des projet dans la timeline

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Timeline.css'
+import React from "react";
+import "./Timeline.css";
+import Projet from "../projets/Projet";
+import ShabbatHome from "../images/home.png";
 
 function Timeline() {
+  const projects = [
+    {
+      title: "Shabbat Brunch",
+      description: "A Shabbat Brunch App",
+      image: ShabbatHome,
+    },
+    {
+      title: "Shabbat Brunch",
+      description: "A Shabbat Brunch App",
+      image: ShabbatHome,
+    },
+    {
+      title: "Shabbat Brunch",
+      description: "A Shabbat Brunch App",
+      image: ShabbatHome,
+    },
+  ];
+
   return (
-    <div>
-      timeline
+    <div className="timeline-container">
+      <div className="timeline">
+        {projects.map((project, index) => (
+          <Projet key={index} {...project} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Timeline
+export default Timeline;
