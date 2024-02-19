e44c6020024c0f3e385a1880d657fc4e69248516
gaouimounir <mounir.gaoui@outlook.com>
2024-02-19 20:43:38 +0100

    feat: :fire: update style presentation

--- src/header/Header.js
+++ src/header/Header.js
@@ -12,26 +12,38 @@ function Header() {
         <div className="header__contact">
           <a href="https://www.linkedin.com/in/am-gaoui/">
             <i className="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="mailto:am.gaoui@protonmail.com">
-            <i className="fa-solid fa-envelope"></i>
+            <p className="name__contact">LinkedIn</p>
           </a>
           <a href="https://github.com/gaouimounir">
             <i className="fa-brands fa-github"></i>
+            <p className="name__contact">Github</p>
+          </a>
+          <a href="mailto:am.gaoui@protonmail.com">
+            <i className="fa-solid fa-envelope"></i>
+            <p className="name__contact">Email</p>
           </a>
         </div>
       </div>
       <div className="header__presentation">
         <div className="header__name">
-          <h1>GAOUI MOUNIR</h1>
+          <h1>Gaoui Mounir</h1>
         </div>
         <div className="header__metier">
-          <h2>WEB DEVELOPER</h2>
+          <h3>📱 WEB DEVELOPER 💻</h3>
         </div>
         <div className="header__description">
           <p>
-            I am a Web Developer with 3 years of experience. I am currently
-            working on my portfolio.
+            💡 Passionate developer, I am ready to put my technical precision
+            and determination at the service of your company.
+          </p>
+          <p>
+            🧠 My thirst for learning and ability to adapt quickly make me a
+            driving force within your team.
+          </p>
+          <p>
+            🎯 With a background in projects and hands-on practices, I am
+            prepared to tackle technical challenges and actively contribute to
+            your projects.
           </p>
         </div>
       </div>
