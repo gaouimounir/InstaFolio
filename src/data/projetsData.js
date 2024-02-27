35006c9b9daed5f3603168084443a1d595cf017f
gaouimounir <mounir.gaoui@outlook.com>
2024-02-27 23:46:10 +0100

    feat: traduction des descriptions de projet

--- src/data/projetsData.js
+++ src/data/projetsData.js
@@ -17,54 +17,51 @@ const projetsData = [
     title: "Shabbat Brunch",
     description: (
       <div>
-        Shabbat Brunch est un projet complet de développement web réalisé en
-        utilisant la technologie C# .NET avec l'architecture MVC et la gestion
-        de base de données via Entity Framework. Il s'agit d'un site dédié à un
-        restaurant, offrant une expérience utilisateur interactive et pratique.
+        Shabbat Brunch is a web development project implemented using C# .NET
+        technology with MVC architecture and database management through Entity
+        Framework. It is a website dedicated to a restaurant, providing an
+        interactive and convenient user experience.
         <br />
         <br />
-        <strong>Fonctionnalités Principales :</strong>
+        <strong>Main Features:</strong>
         <ul>
           <li>
-            Consultation de la Carte : Les utilisateurs peuvent explorer la
-            carte détaillée du restaurant, affichant une variété de plats et de
-            boissons proposés.
+            Menu Exploration: Users can explore the detailed restaurant menu,
+            displaying a variety of offered dishes and drinks.
           </li>
           <li>
-            Réservation en Ligne : Intégration d'un système de réservation en
-            ligne permettant aux clients de réserver leur table à l'avance,
-            améliorant ainsi l'efficacité et la satisfaction de la clientèle.
+            Online Reservation: Integration of an online reservation system
+            allowing customers to book their table in advance, enhancing
+            efficiency and customer satisfaction.
           </li>
           <li>
-            Gestion Admin : Interface d'administration sécurisée pour gérer
-            facilement les réservations, mettre à jour le menu et gérer les
-            informations de contact du restaurant.
+            Admin Management: Secure administration interface to easily manage
+            reservations, update the menu, and handle restaurant contact
+            information.
           </li>
           <li>
-            Horaires et Localisation : Affichage clair des horaires d'ouverture
-            du restaurant et accès à des informations précises sur sa
-            localisation.
+            Timings and Location: Clear display of restaurant opening hours and
+            access to precise information about its location.
           </li>
           <li>
-            Base de Données SQLite : Utilisation d'une base de données SQLite
-            pour stocker et récupérer les données du site de manière efficace et
-            sécurisée.
+            SQLite Database: Use of an SQLite database to efficiently and
+            securely store and retrieve website data.
           </li>
         </ul>
-        <strong>Technologies Utilisées :</strong>
+        <strong>Technologies Used:</strong>
         <ul>
           <li>C# .NET</li>
           <li>ASP.NET MVC</li>
           <li>Entity Framework</li>
           <li>SQLite</li>
         </ul>
-        ShabbatBrunch démontre une application pratique des technologies web
-        modernes, offrant une solution fonctionnelle pour les restaurants qui
-        cherchent à améliorer leur présence en ligne et à faciliter les
-        interactions avec leurs clients.
+        ShabbatBrunch demonstrates a practical application of modern web
+        technologies, providing a functional solution for restaurants looking to
+        enhance their online presence and facilitate interactions with
+        customers.
         <br />
         <br />
-        <strong>Site Web : </strong>
+        <strong>Website: </strong>
         <a
           style={{ color: "blue" }}
           href="https://lashabbatapp.azurewebsites.net"
@@ -83,54 +80,111 @@ const projetsData = [
   //   mainImage: MeteoConnect,
   //   images: [MeteoConnect, MeteoBody],
   // },
+
+  {
+    id: 2,
+    title: "DevBank",
+    description: (
+      <div>
+        The DevBank project is a simplified banking application developed in C#
+        language with the .NET framework. The main objective of this application
+        is to simulate basic banking operations for two types of accounts: a
+        current account (CompteCourant) and a savings account (CompteEpargne).
+        <br />
+        <br />
+        <strong>Main Features:</strong>
+        <ul>
+          <li>
+            Deposit and Withdrawal: Users can make deposits and withdrawals from
+            their accounts.
+          </li>
+          <li>
+            Transfer: The transfer function allows the transfer of money between
+            two accounts.
+          </li>
+          <li>
+            Transaction History: Users can view the history of their
+            transactions over the last 30 days.
+          </li>
+          <li>
+            Account Policy: Current and savings accounts have specific policies,
+            such as withdrawal fees, authorized overdraft, interest rate, etc.
+          </li>
+          <li>
+            Notifications: The NotificationManager class manages notifications
+            and records them in a history file.
+          </li>
+        </ul>
+        <strong>Technologies Used:</strong>
+        <ul>
+          <li>C# .NET</li>
+          <li>Visual Studio</li>
+          <li>N Unit</li>
+        </ul>
+        DevBank represents a simulation of a banking system with an
+        object-oriented implementation, offering basic functionalities for
+        managing current and savings bank accounts.
+        <br />
+        <br />
+        <strong>Github Repository: </strong>
+        <a
+          style={{ color: "blue" }}
+          href="https://lashabbatapp.azurewebsites.net"
+        >
+          https://lashabbatapp.azurewebsites.net
+        </a>
+      </div>
+    ),
+    mainImage: FridgeHome,
+    images: [FridgeHome, FridgeApp],
+  },
+
   {
     id: 3,
     title: "Ducsman",
     description: (
       <div>
-        Le projet Ducsman est une réalisation passionnante qui incarne
-        l'alliance de la créativité et de la fonctionnalité dans le domaine du
-        commerce électronique. Développé en JavaScript, ce site offre une
-        expérience de shopping en ligne immersive pour les amateurs de mode.
+        The Ducsman project is an exciting achievement that embodies the
+        alliance of creativity and functionality in the e-commerce domain.
+        Developed in JavaScript, this site offers an immersive online shopping
+        experience for fashion enthusiasts.
         <br />
         <br />
-        <strong>Fonctionnalités Principales :</strong>
+        <strong>Main Features:</strong>
         <ul>
           <li>
-            Catalogue Étendu : Découvrez un large éventail de vêtements triés
-            par catégories, offrant aux utilisateurs une variété de choix selon
-            leurs préférences.
+            Extended Catalog: Explore a wide range of clothing categorized,
+            providing users with a variety of choices based on their
+            preferences.
           </li>
           <li>
-            Tri et Filtrage : La fonctionnalité de tri et de filtrage permet aux
-            visiteurs de rechercher rapidement des articles spécifiques,
-            facilitant ainsi leur expérience de navigation.
+            Sorting and Filtering: The sorting and filtering feature allows
+            visitors to quickly search for specific items, facilitating their
+            browsing experience.
           </li>
           <li>
-            Gestion du Panier : Les utilisateurs peuvent ajouter des articles à
-            leur panier virtuel, facilitant la planification de leurs achats.
+            Cart Management: Users can add items to their virtual cart,
+            facilitating planning for their purchases.
           </li>
         </ul>
-        <strong> Technologies Utilisées : </strong>
+        <strong>Technologies Used:</strong>
         <ul>
           <li>
-            JavaScript : Le cœur du site repose sur JavaScript, assurant une
-            interactivité dynamique pour les utilisateurs.
+            JavaScript: The core of the site relies on JavaScript, ensuring
+            dynamic interactivity for users.
           </li>
           <li>
-            Données JSON : Les informations sur les produits sont stockées de
-            manière organisée dans des fichiers JSON, garantissant une gestion
-            efficace des données.
+            JSON Data: Product information is organized in JSON files,
+            guaranteeing efficient data management.
           </li>
         </ul>
-        Le projet Ducsman représente l'intersection de la mode et de la
-        technologie, démontrant la capacité à créer des solutions numériques
-        innovantes dans le secteur du commerce électronique. Explorez les
-        dernières tendances et préparez-vous à une expérience de shopping en
-        ligne exceptionnelle avec Ducsman.
+        The Ducsman project represents the intersection of fashion and
+        technology, showcasing the ability to create innovative digital
+        solutions in the e-commerce sector. Explore the latest trends and
+        prepare for an exceptional online shopping experience with Ducsman.
         <br />
         <br />
-        <strong>Site Web : </strong>
+        <strong>Website: </strong>
         <a
           style={{ color: "blue" }}
           href="https://gaouimounir.github.io/eCommerceJS"
@@ -147,55 +201,54 @@ const projetsData = [
     title: "Ola Bel Bul",
     description: (
       <div>
-        Le projet Ola Bel Bul incarne une fusion harmonieuse entre la créativité
-        digitale et le lancement d'une nouvelle boisson probiotique. Développé
-        en HTML, CSS et JavaScript, ce site vitrine offre une plongée immersive
-        dans l'univers de cette entreprise innovante de kombucha.
+        The Ola Bel Bul project embodies a harmonious fusion of digital
+        creativity and the launch of a new probiotic beverage. Developed in
+        HTML, CSS, and JavaScript, this showcase site offers an immersive dive
+        into the world of this innovative kombucha company.
         <br />
         <br />
-        <strong> Caractéristiques Clés : </strong>
+        <strong>Main Features:</strong>
         <ul>
           <li>
-            Histoire et Valeurs : Explorez l'histoire captivante de Ola Bel Bul,
-            découvrez ses valeurs fondamentales et plongez dans le parcours qui
-            a conduit à la création de cette boisson unique.
+            Story and Values: Explore the captivating story of Ola Bel Bul,
+            discover its core values, and dive into the journey that led to the
+            creation of this unique beverage.
           </li>
           <li>
-            Diversité des Saveurs : Présentation des différentes saveurs de
-            kombucha proposées, offrant aux visiteurs un aperçu des choix
-            délicieux disponibles.
+            Flavor Diversity: Presentation of the different flavors of kombucha
+            offered, giving visitors an overview of the delicious choices
+            available.
           </li>
           <li>
-            Prestations Offertes : Une section détaillée sur les services
-            proposés par Ola Bel Bul, mettant en lumière les options de
-            partenariat, les événements spéciaux et bien plus encore.
+            Offered Services: A detailed section on the services offered by Ola
+            Bel Bul, highlighting partnership options, special events, and much
+            more.
           </li>
           <li>
-            Page de Contact : Facilitez la communication avec une page de
-            contact interactive, permettant aux visiteurs d'envoyer des messages
-            et de poser des questions.
+            Contact Page: Facilitate communication with an interactive contact
+            page, allowing visitors to send messages and ask questions.
           </li>
         </ul>
-        <strong>Technologies Utilisées : </strong>
+        <strong>Technologies Used:</strong>
         <ul>
           <li>
-            HTML, CSS, JavaScript : La combinaison de ces technologies assure
-            une présentation visuelle attrayante et une interaction fluide avec
-            le site.
+            HTML, CSS, JavaScript: The combination of these technologies ensures
+            an attractive visual presentation and smooth interaction with the
+            site.
           </li>
           <li>
-            Design Responsive : L'interface est pensée pour s'adapter à tous les
-            types d'appareils, garantissant une expérience utilisateur optimale
-            sur ordinateurs, tablettes et smartphones.
+            Responsive Design: The interface is designed to adapt to all types
+            of devices, ensuring an optimal user experience on computers,
+            tablets, and smartphones.
           </li>
         </ul>
-        Ola Bel Bul représente bien plus qu'une simple boisson, c'est une
-        aventure gustative ancrée dans une histoire inspirante. Explorez les
-        saveurs, plongez dans l'histoire et découvrez les opportunités de
-        collaboration avec ce site web captivant.
+        Ola Bel Bul represents more than just a beverage; it is a flavorful
+        adventure rooted in an inspiring story. Explore the flavors, delve into
+        the story, and discover collaboration opportunities with this engaging
+        website.
         <br />
         <br />
-        <strong>Site Web : </strong>
+        <strong>Website: </strong>
         <a
           style={{ color: "blue" }}
           href="https://gaouimounir.github.io/WebSite_OlaBelBul"
